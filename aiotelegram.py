aa69afa65a790bfd825fd9f702b5e98bb2f51dff
pohmelie <multisosnooley@gmail.com>
2016-07-13 16:48:18 +0300

    init commit

---
+++ aiotelegram.py
@@ -0,0 +1,52 @@
+import asyncio
+import functools
+
+import aiohttp
+
+
+__version__ = "0.1.0"
+
+
+class Api:
+
+    URL = "https://api.telegram.org/bot{token}/{method}"
+
+    def __init__(self, token, *, connector_factory=lambda **_: None,
+                 loop=None, pause=0.05):
+
+        self.token = token
+        self.connector_factory = connector_factory
+        self.pause = pause
+        self.loop = loop or asyncio.get_event_loop()
+        self.last_call = self.loop.time()
+        self.offset = 0
+
+    async def _api_call(self, method, **options):
+
+        now = self.loop.time()
+        delta = self.pause - (now - self.last_call)
+        await asyncio.sleep(max(0, delta), loop=self.loop)
+        self.last_call = now
+
+        request = aiohttp.get(
+            str.format(Api.URL, token=self.token, method=method),
+            connector=self.connector_factory(loop=self.loop),
+            loop=self.loop,
+            data=options,
+        )
+        async with request as response:
+
+            return await response.json()
+
+    def __getattr__(self, method):
+
+        return functools.partial(self._api_call, str.replace(method, "_", ""))
+
+    async def get_updates(self):
+
+        response = await self.getUpdates(offset=self.offset)
+        for update in response["result"]:
+
+            self.offset = max(self.offset, update["update_id"] + 1)
+
+        return response
---
+++ readme.md
@@ -0,0 +1,43 @@
+# aiotelegram
+Tiny asyncio-based telgram api wrapper library. Features:
+* Simple as telegram api is.
+* Based on [`aiohttp`](https://github.com/KeepSafe/aiohttp).
+* Proxy available (via [`aiohttp`](https://github.com/KeepSafe/aiohttp) [`ProxyConnector`](http://aiohttp.readthedocs.io/en/stable/client_reference.html#aiohttp.ProxyConnector)).
+* `snake_case` api converted to telegram `camelCase`.
+* Polling `offset` handled for you via `get_updates` method.
+* Source code is [tiny as your screen]().
+
+## Usage
+### Polling updates
+```python
+import aiotelegram
+
+
+async def ...(...):
+
+    api = aiotelegram.Api(token)
+    while True:
+
+        response = await api.get_updates()
+        if not response["ok"]:
+
+            ...
+
+        else:
+
+            for update in response["result"]:
+
+                ...
+
+        await asyncio.sleep(delay)
+```
+### Sending message
+```python
+async def ...(...):
+
+    await api.send_message(
+        chat_id=123456,
+        text="*foobar*",
+        parse_mode="Markdown",
+    )
+```
